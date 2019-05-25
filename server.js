1ab0fe48a8125bc9d913059084f168fb0284d76b
Asu-nyan <45885583+Asu-nyan@users.noreply.github.com>
2019-05-25 17:12:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const password = process.env.MONGO_PW;
 mongoose.connect(`mongodb+srv://${user}:${password}@bs-projects-auzep.mongodb.net/test?retryWrites=true`, {useNewUrlParser: true});
 const schema = new mongoose.Schema({ content: 'string', date: 'string', created_at: 'string' });
 const List = mongoose.model('List', schema);
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     List.find({}, (err, lists) => {
@@ -17,7 +18,7 @@ app.get('/', (req, res) => {
         links = uniq.map(li => {
             return `<a href="/${li}">Listen vom ${li}</a>`;
         });
-        res.send(links.join('<br>'));
+        res.send(style(links.join('<br>')));
     });
 });
 
@@ -30,8 +31,56 @@ app.get('/:date', (req, res) => {
             html += li.content.replace('\n', '<br>');
             html += '<br><hr><br>';
         });
-        res.send(html);
+        res.send(style(html));
     });
 });
 
 app.listen(port, () => console.log('Listening on Port ' + port));
+
+function style(content) {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="/style.css">
+        <title>WebCC</title>
+        <style>
+        * {
+            cursor: default !important;
+        }
+        a {
+            text-decoration: none;
+        }
+        </style>
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <a class="navbar-brand" href="/">WebCC</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01"
+                aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarColor01">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="/">Home</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <br>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    ${content}
+                </div>
+                <div class="col-md-8"></div>
+            </div>
+        </div>
+    </body>
+    </html>
+    `;
+}
